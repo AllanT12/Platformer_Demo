f6915f842b66bdb6dfde8d654d7029a2e434c635
AllanT12 <50295250+AllanT12@users.noreply.github.com>
2020-04-15 23:53:53 +0300

    myFirstProject

---
+++ test2D/Assets/enemy_graph.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class enemy_graph : MonoBehaviour
+{
+    public AIPath aipath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aipath.desiredVelocity.x>=0.01f)
+        {
+            transform.localScale = new Vector3(-3f, 3f, 1f);
+        }
+        else if(aipath.desiredVelocity.x<=-0.01f)
+        {
+            transform.localScale = new Vector3(3f, 3f, 1f);
+        }
+    }
+}
---
+++ test2D/Assets/player_movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public Animator animator;
+
+    float horizontalMovement = 0f;
+
+    public float speedrun = 40f;
+
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement=Input.GetAxisRaw("Horizontal") * speedrun;
+
+        animator.SetFloat("Speed",Mathf.Abs(horizontalMovement));
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping",true);
+        }
+        
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping",false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime,false,jump);
+        jump = false;
+    }
+}
